117e8760f3a7748c3cbb5ab62b8274793cfd921b
Ning Sun <sunng@protonmail.com>
2018-11-04 16:34:10 +0800

    (test) added test and update deps

---
+++ src/test/java/io/github/espresso4j/jettino/JettinoAdapterTest.java
@@ -0,0 +1,20 @@
+package io.github.espresso4j.jettino;
+
+import io.github.espresso4j.espresso.Response;
+import org.junit.Test;
+
+public class JettinoAdapterTest {
+
+    @Test
+    public void test() throws Exception {
+        BaseAdapterOptions options = new BaseAdapterOptions();
+        options.setJoin(false);
+        options.setPort(32080);
+
+        JettinoAdapter jetty = new JettinoAdapter(options);
+
+        jetty.start((req) -> Response.of(200));
+        jetty.stop();
+    }
+
+}
